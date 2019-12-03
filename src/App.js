9105e5744fd65565eeae58bdc6b2c33acd0f240e
Ashley Alexander-Lee <asalexanderlee@gmail.com>
2019-12-03 16:28:17 -0500

    Initial react files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Header from './Header.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    curPg: "home"
+  }
+  onPgChoice = e => {
+    this.setState({curPg: e.target.id});
+  }
+  render(){
+    return (
+      <div className="App">
+        <Header curPg={this.state.curPg} onPgChoice={this.onPgChoice} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+const Header = ({curPg, onPgChoice}) => {
+  return(
+    <div class="Header">
+        <div class="name">Ashley Alexander-Lee</div>
+        <div class="menu">
+          <div class="menu-item" onClick={onPgChoice} id="home">Home</div>
+          <div class="menu-item" onClick={onPgChoice} id="resume">Resume</div>
+          <div class="menu-item" onClick={onPgChoice} id="portfolio">Portfolio</div>
+          <div class="menu-item" onClick={onPgChoice} id="contact">Contact</div>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
